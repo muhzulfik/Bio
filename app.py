6e8cadf6162f6ace6014f927d2054bdab8899465
Muhamad Zulfikri <72639811+muhzulfik@users.noreply.github.com>
2021-06-08 20:38:39 +0700

    Add files via upload
    commit main flask

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import render_template
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
